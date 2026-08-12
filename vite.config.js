206896d9ee37309699f3a40d591154433eb73e79
Francis Ruta <Francis.Ruta87@gmail.com>
2026-08-12 15:15:53 +0800

    Finish filling up personal details

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // IMPORTANT: Change this to match your GitHub repo name.
   // If deploying to user page https://username.github.io/ leave as '/'
-  base: '/portfolio/',
-})
+  // If deploying to a project page https://username.github.io/<repo>/ set to '/<repo>/'
+  base: "/online-portfolio/",
+});
